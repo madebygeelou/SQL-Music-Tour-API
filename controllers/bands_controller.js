2310d2e02331e95bd3ff40789974e7e3de765616
madebygeelou <125709909+madebygeelou@users.noreply.github.com>
2024-05-16 12:46:57 -0700

    finished routes

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -3,9 +3,15 @@ const bands = require('express').Router()
 const db = require('../models')
 const { Band } = db 
 
+//INDEX
 bands.get('/', async (req,res) => {
     try{
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC']],
+            where: {
+                name: { [Op.like]: '%${req.query.name ? req.query.name : ``}%'}
+            }
+        })
         res.status(200).json(foundBands)
     }catch(error){
         res.status(500).json(error)
